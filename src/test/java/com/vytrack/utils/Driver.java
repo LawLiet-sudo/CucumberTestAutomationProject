677211e25bd771ea13b121198b915dba87cd418d
LawLiet-sudo <lowlite734@gmail.com>
2020-11-10 20:41:52 -0500

    driverAdd

--- src/test/java/com/vytrack/utils/Driver.java
+++ src/test/java/com/vytrack/utils/Driver.java
@@ -13,13 +13,24 @@ import java.net.URL;
 
 public class Driver {
 
+
+
     private static WebDriver driver;
-
-    private Driver() {}
-
+    private Driver() {
+    }
     public static WebDriver getDriver() {
         if (driver == null) {
             String browser = ConfigurationReader.getProperty("browser");
+//            jenkins command: test -Dcucumber.filter.tags="@smoke" -Dbrowser="chrome"
+//            custom environment variables: -Dbrowser
+//            -Dproperty  = then read in java System.getProperty("property")
+//            if env variable was specified
+            if (System.getProperty("browser") != null) {
+//                then change browser type
+//                regardless on value configuration.properties
+                System.out.println("Browser type was changed to: " + System.getProperty("browser"));
+                browser = System.getProperty("browser");
+            }
             switch (browser) {
                 case "chrome":
                     WebDriverManager.chromedriver().setup();
@@ -29,14 +40,15 @@ public class Driver {
                     WebDriverManager.firefoxdriver().setup();
                     driver = new FirefoxDriver();
                     break;
-
                 case "remote-chrome":
                     try {
-                          ChromeOptions chromeOptions = new ChromeOptions();
+//                    ChromeOptions chromeOptions = new ChromeOptions();
+                        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+                        desiredCapabilities.setBrowserName("chrome");
 
                         URL gridUrl = new URL("http://54.161.156.5:4444/grid/wd/hub");
 
-                        driver = new RemoteWebDriver(gridUrl, chromeOptions);
+                        driver = new RemoteWebDriver(gridUrl, desiredCapabilities);
                     }catch(Exception e){
                         e.printStackTrace();
                     }
