5ea805dc4b9c32316b82609990c48b6d7d492520
LawLiet-sudo <lowlite734@gmail.com>
2020-11-14 20:57:13 -0500

    jenkins1

--- src/test/java/com/vytrack/runners/CucumberRunner.java
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/vytrack/step_definitions",
         dryRun = false,
-        tags = "@add_car_scenario_outline",
+        tags = "@saleslogin",
         publish = true
 )
 
